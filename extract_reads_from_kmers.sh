b3d3e2905a59d998012e544b982855a8d2672ded
dannyesselink <danny.esselink@wur.nl>
2019-03-21 10:44:11 +0100

    Update extract_reads_from_kmers.sh

--- extract_reads_from_kmers.sh
+++ extract_reads_from_kmers.sh
@@ -1,4 +1,4 @@
-### reformat the kmer table
+### reformat the kmer table for use in script kfastqfilter
 ### IN = the kmer table 
 
 IN=$1
@@ -9,10 +9,11 @@ COUNT=`grep -v "[#N]" $OUT|wc -l`
 SUM=`awk '{ sum+=$2} END {print sum}' $OUT`
 echo -e "+\t$SUM\t$COUNT\t31" >> $OUT
 
-### extracting the reads
+### extracting the reads using script kfastqfilter
 
 kfastqfilter ${OUT} \
 read1.fq read2.fq /dev/null /dev/null \
 extracted_read1.fq extracted_read2.fq 1 
 
 ### the last number = how many kmers of the list the read must have at least
+### see further explanantions by executing kfastqfilter
